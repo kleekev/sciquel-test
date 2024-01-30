c99aa7291294e32045773358db83b0bcdee17896
Kevin Lee <kleekev@sas.upenn.edu>
2024-01-29 22:35:03 -0500

    Filled in apis

--- src/app/api/comments/route.ts
+++ src/app/api/comments/route.ts
@@ -1,13 +1,37 @@
 import { NextRequest, NextResponse } from "next/server";
+import { PrismaClient } from '@prisma/client'
 
+const prisma = new PrismaClient()
 
 export async function GET(req: NextRequest){
     // fill here in
-
-    return NextResponse.json({ test: "success", }, { status: 200 })
+    try {
+        const recentComments = await prisma.comment.findMany({
+            take: 50,
+            orderBy: {
+                createdAt: 'desc',
+            },
+        });
+        return NextResponse.json({ comments: recentComments }, { status: 200 });
+    } catch (error) {
+        return NextResponse.json({ error: error }, { status: 400 });
+    }    
 }
 
 export async function POST(req: NextRequest){
     // fill here in
+    const { email, name, comment } = await req.json();
+    
+    try {
+        const comm = await prisma.comment.create({
+            data: {
+                email: email,
+                name: name,
+                comment: comment
+            },
+        })
+    } catch (error) {
+        return NextResponse.json({ error : error, }, { status: 400 })
+    }
     return NextResponse.json({ test: "success", }, { status: 200 })
 }
